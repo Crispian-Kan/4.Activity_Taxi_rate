9f4343bd6fe98041702e21a3850f1f7d0abead7b
Jim-Kan <tmpss0909889@gmail.com>
2020-11-12 01:05:16 +0800

    taxi rate 1.2
    change variable

--- Project_Taxi_rate_calculation 拷貝/app/src/main/java/com/example/project_taxi_rate_calculation/MainActivity.java
+++ Project_Taxi_rate_calculation 拷貝/app/src/main/java/com/example/project_taxi_rate_calculation/MainActivity.java
@@ -17,29 +17,29 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button button = (Button)findViewById(R.id.btn1);
-        button.setOnClickListener(the_texi);
+        Button button = (Button)findViewById(R.id.count);
+        button.setOnClickListener(texi_rate);
     }
-    private View.OnClickListener the_texi = new View.OnClickListener() {
+    private View.OnClickListener texi_rate = new View.OnClickListener() {
         @Override
         public void onClick(View v) {
             DecimalFormat nf = new DecimalFormat("0");
-            EditText findcount = (EditText)findViewById(R.id.edit1);
-            EditText findcount1 = (EditText)findViewById(R.id.edit2);
-            EditText findcount2 = (EditText)findViewById(R.id.edit3);
+            EditText instance = (EditText)findViewById(R.id.input_instance);
+            EditText set_instance = (EditText)findViewById(R.id.set_instance_rate);
+            EditText input_fee = (EditText)findViewById(R.id.input_fee);
 
             int basic = 75;
             //距離
-            double distance = Double.parseDouble(findcount.getText().toString());
-            double basicCount = Double.parseDouble(findcount1.getText().toString());
-            double addCost = Double.parseDouble(findcount2.getText().toString());
+            double distance = Double.parseDouble(instance.getText().toString());
+            double basicCount = Double.parseDouble(set_instance.getText().toString());
+            double addCost = Double.parseDouble(input_fee.getText().toString());
             //費用計算
             double count = basic + ((distance / basicCount) * addCost);
 
-            TextView cost = (TextView)findViewById(R.id.textV4);
+            TextView cost = (TextView)findViewById(R.id.output);
             //里程低於250公尺時
             if (distance < 250){
-                cost.setText("80 元");
+                cost.setText(nf.format(basic+addCost)+"元");
             }//每250公尺時且費率加收5元
             else if(distance >= 250 && addCost >=5){
                 cost.setText(nf.format(count) + "元");
